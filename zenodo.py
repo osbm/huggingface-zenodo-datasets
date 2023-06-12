a822dfadf0b363369e2abb0e6e58e5a8d37c63f7
osbm <osmanfbayram@gmail.com>
2023-06-12 13:27:48 +0300

    test_this out

--- zenodo.py
+++ zenodo.py
@@ -7,4 +7,103 @@ import os
 
 import datasets
 
-print("heyoooo")
+_DESCRIPTION = """\
+This dataset is for downloading a Zenodo dataset without extra packages.
+"""
+
+
+
+
+class NewDataset(datasets.GeneratorBasedBuilder):
+    """This dataset downloads a zenodo dataset and returns its path."""
+
+    VERSION = datasets.Version("1.1.0")
+
+    # This is an example of a dataset with multiple configurations.
+    # If you don't want/need to define several sub-sets in your dataset,
+    # just remove the BUILDER_CONFIG_CLASS and the BUILDER_CONFIGS attributes.
+
+    # If you need to make complex sub-parts in the datasets with configurable options
+    # You can create your own builder configuration class to store attribute, inheriting from datasets.BuilderConfig
+    # BUILDER_CONFIG_CLASS = MyBuilderConfig
+
+    # You will be able to load one or the other configurations in the following list with
+    # data = datasets.load_dataset('my_dataset', 'first_domain')
+    # data = datasets.load_dataset('my_dataset', 'second_domain')
+    # BUILDER_CONFIGS = [
+    #     datasets.BuilderConfig(name="first_domain", version=VERSION, description="This part of my dataset covers a first domain"),
+    #     datasets.BuilderConfig(name="second_domain", version=VERSION, description="This part of my dataset covers a second domain"),
+    # ]
+
+    # DEFAULT_CONFIG_NAME = "first_domain"  # It's not mandatory to have a default configuration. Just use one if it make sense.
+
+    def _info(self):
+        # TODO: This method specifies the datasets.DatasetInfo object which contains informations and typings for the dataset
+        # if self.config.name == "first_domain":  # This is the name of the configuration selected in BUILDER_CONFIGS above
+        #     features = datasets.Features(
+        #         {
+        #             "sentence": datasets.Value("string"),
+        #             "option1": datasets.Value("string"),
+        #             "answer": datasets.Value("string")
+        #             # These are the features of your dataset like images, labels ...
+        #         }
+        #     )
+        # else:  # This is an example to show how to have different features for "first_domain" and "second_domain"
+        #     features = datasets.Features(
+        #         {
+        #             "sentence": datasets.Value("string"),
+        #             "option2": datasets.Value("string"),
+        #             "second_domain_answer": datasets.Value("string")
+        #             # These are the features of your dataset like images, labels ...
+        #         }
+        #     )
+
+        features = datasets.Features(
+            {
+                "path": datasets.Value("string"),
+            }
+        )
+
+        return datasets.DatasetInfo(
+            # This is the description that will appear on the datasets page.
+            description=_DESCRIPTION,
+            # This defines the different columns of the dataset and their types
+            features=features,  # Here we define them above because they are different between the two configurations
+            # If there's a common (input, target) tuple from the features, uncomment supervised_keys line below and
+            # specify them. They'll be used if as_supervised=True in builder.as_dataset.
+            # supervised_keys=("sentence", "label"),
+            # Homepage of the dataset for documentation
+            # homepage=_HOMEPAGE,
+            # License for the dataset if available
+            # license=_LICENSE,
+            # Citation for the dataset
+            # citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        # TODO: This method is tasked with downloading/extracting the data and defining the splits depending on the configuration
+        # If several configurations are possible (listed in BUILDER_CONFIGS), the configuration selected by the user is in self.config.name
+
+        # dl_manager is a datasets.download.DownloadManager that can be used to download and extract URLS
+        # It can accept any type or nested list/dict and will give back the same structure with the url replaced with path to local files.
+        # By default the archives will be extracted and a path to a cached folder where they are extracted is returned instead of the archive
+        zenodo_id = self.config.name.split("/")[0]
+        filename = self.config.name.split("/")[1]
+
+        url = f"https://zenodo.org/record/{zenodo_id}/files/{filename}"
+
+        data_dir = dl_manager.download_and_extract([url])
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                data_dir=data_dir[0],
+            ),
+        ]
+
+    # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
+    def _generate_examples(self, data_dir):
+        # TODO: This method handles input defined in _split_generators to yield (key, example) tuples from the dataset.
+        # The `key` is for legacy reasons (tfds) and is not important in itself, but must be unique for each example.
+        
+        yield "path", data_dir
+        
